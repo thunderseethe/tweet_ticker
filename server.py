73c82baae701a826d1f2b26da6254efeaefb72ff
thunderseethe <emsmith1995@gmail.com>
2017-04-18 17:17:41 -0400

    Wrapper senitment analyzer in REST api endpoint

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
+#from nltk.classify import NaiveBayesClassifier
+#from nltk.corpus import subjectivity
+#from nltk.sentiment import SentimentAnalyzer
+#from nltk.sentiment.util import *
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+#from nltk import tokenize
+#from nltk.twitter import Twitter
 from flask import Flask
 from flask import render_template
 from twython import Twython
-
+import json
 app = Flask(__name__)
 
 
@@ -17,13 +24,43 @@ def hello_world():
 @app.route('/query/<string:query>')
 def query(query):
 	twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
-	results = twitter.search(q='$'+query, count=100)
+	tweets = searchTwitter(twitter, query)
+	values, percPos, percNeg = analyzeTweets(tweets)
+	return json.dumps({
+		"data": list(zip(tweets, values)),
+		"percentPositive": percPos,
+		"percentNegative": percNeg
+	}, indent=0)	
+	
 
-	tweetText = [tweet['text'] for tweet in results['statuses']]
-	resultsLength = len(results['statuses'])
-	lastID = results['statuses'][-1]['id_str']
+def searchTwitter(twitter, query, length=2000):
+	tweetText = []
+	resultsLength = 100
+	lastID = None
 
-	while resultsLength == 100 and len(tweetText) < 1998:
-		results = twitter.search(q=query, count=100, max_id=lastID)
-		
+	while resultsLength == 100 and len(tweetText) < length - 2:
+		results = twitter.search(q='$'+query, count=100, max_id=lastID)
+		tweetText.extend([tweet['text'] for tweet in results['statuses']])
+		resultsLength = len(results['statuses'])
+		lastID = results['statuses'][-1]['id_str']
 
+	return tweetText
+
+def tweetsToSentiments(sentences, sid=SentimentIntensityAnalyzer()):
+	return [sid.polarity_scores(sentence)["compound"] for sentence in sentences]
+
+def analyzeTweets(tweets):
+	values = tweetsToSentiments(tweets)
+	positiveCount = 0
+	negativeCount = 0
+
+	for value in values:
+		if value > 0:
+			positiveCount += 1
+		elif value < 0:
+			negativeCount += 1
+
+	percentPositive = (positiveCount * 100) / len(values)
+	percentNegative = (negativeCount * 100) / len(values)
+
+	return (values, percentPositive, percentNegative)
